a47eab6a4553da9e3577c943a2feb5cd312b81aa
Vishal Kosaraju <77464839+vishy111@users.noreply.github.com>
2026-07-16 01:13:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 console.log("JS loaded");
 
 d3.csv("AAPL.csv").then(function(data) {
+
+  const parseDate = d3.timeParse("%m/%d/%Y");
+
+  data.forEach(function(d) {
+    d.date = parseDate(d.Date_col?.trim());
+    d.close = +d["Close(t)"]?.trim();
+  });
+
   console.log("COLUMN NAMES:", Object.keys(data[0]));
   console.log("FIRST ROW:", data[0]);
+
+  const w = 800;
+  const h = 400;
+  const margin = { top: 40, right: 40, bottom: 40, left: 60 };
+
+  const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  const x = d3.scaleTime()
+    .domain(d3.extent(data, d => d.date))
+    .range([margin.left, w - margin.right]);
+
+  const y = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.close))
+    .range([h - margin.bottom, margin.top]);
+
+  const line = d3.line()
+    .x(d => x(d.date))
+    .y(d => y(d.close));
+
+  svg.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 2)
+    .attr("d", line);
+
+  svg.append("g")
+    .attr("transform", `translate(0,${h - margin.bottom})`)
+    .call(d3.axisBottom(x));
+
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y));
+
 });
